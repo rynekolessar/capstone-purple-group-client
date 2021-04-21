38dd7ad8401380c4ed57abfee63bdd6d32b526ca
rynekolessar <ryne.kolessar@gmail.com>
2021-04-21 15:29:51 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CreateGame from './components/CreateGame';
+import ShowGameList from './components/ShowGameList';
+import ShowGameDetails from './components/ShowGameDetails';
+import UpdateGameInfo from './components/UpdateGameInfo';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component={ShowGameList} />
+          <Route path='/create-game' component={CreateGame} />
+          <Route path='/edit-game/:id' component={UpdateGameInfo} />
+          <Route path='/show-game/:id' component={ShowGameDetails} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
