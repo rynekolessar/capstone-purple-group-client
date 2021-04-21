9a6d2ffe17c7fee85a769d583274e58693a9c55b
rynekolessar <ryne.kolessar@gmail.com>
2021-04-21 17:28:50 -0500

    fixed game card

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -7,18 +7,18 @@ const GameCard = (props) => {
 
     return (
         <div className="card-container">
-            <img src="" alt="" />
+            <img src="https://image.api.playstation.com/cdn/UP1003/CUSA05657_00/FTkgdf4kd60kZPtiPrIGbqk1g5WaKfF7.png?w=440" alt="" />
             <div className="desc">
                 <h2>
                     <Link to={`/show-game/${game._id}`}>
                         { game.title }
                     </Link>
                 </h2>
-                <h3>{game.game_studio}</h3>
-                <p>{game.description}</p>
+                <h3>{game.platform}</h3>
+                <p>{game.genre}</p>
             </div>
         </div>
-    );
-}
+    )
+};
 
 export default GameCard;
