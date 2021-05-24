0dc5bf9c968bf795b5555ced4fe273af5edda31a
rynekolessar <ryne.kolessar@gmail.com>
2021-05-23 23:53:52 -0500

    Formatting

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,107 +5,90 @@ import axios from 'axios';
 // import authService from '../services/auth.service';
 
 class Login extends Component {
-    constructor() {
-        super();
-        this.state = {
-            email: '',
-            password: '',
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
 
-    onChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const data = {
+      email: this.state.email,
+      password: this.state.password
     };
 
-    onSubmit = e => {
-        e.preventDefault();
+    axios
+      .post('http://localhost:8082/users/login', data)
+      .then(res => {
+        this.setState({
+          email: '',
+          password: ''
+        })
+        localStorage.setItem('user', JSON.stringify(res.data));
+        this.props.history.push('/');
+      })
+      .catch(err => {
+        console.log("Error in Login");
+      })
+  }
 
-        const data = {
-            email: this.state.email,
-            password: this.state.password
-        };
-
-        axios
-            .post('http://localhost:8082/users/login', data)
-            .then(res => {
-                this.setState({
-                    email: '',
-                    password: ''
-                })
-                localStorage.setItem('user', JSON.stringify(res.data));
-                this.props.history.push('/');
-            })
-            .catch(err => {
-                console.log("Error in Login");
-            })
-
-        // authService.login(
-        //     data
-        // ).then(response => {
-        //     this.props.history.push('/');
-        // },
-        //     error => {
-        //         const resMessage =
-        //             (error.response &&
-        //                 error.response.data &&
-        //                 error.response.data.message) ||
-        //             error.message ||
-        //             error.toString();
-        //         this.setState({
-        //             successful: false,
-        //             message: resMessage
-        //         });
-        //     });
-    }
-
-    render() {
-        console.log(localStorage.getItem('user'));
-        return (
-            <div className="Login">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <br />
-                            <h2 className="display-4 text-center">Login</h2>
-                        </div>
-
-                        <div className="col-md-4 m-auto">
-                            <p className="lead text-center">
-
-                            </p>
-                            <form noValidate onSubmit={this.onSubmit}>
-                                <div className='form-group'>
-                                    <input
-                                        type='email'
-                                        placeholder='Email'
-                                        name='email'
-                                        className='form-control'
-                                        value={this.state.email}
-                                        onChange={this.onChange}
-                                    />
-                                </div>
-
-                                <div className='form-group'>
-                                    <input
-                                        type='password'
-                                        placeholder='Password'
-                                        name='password'
-                                        className='form-control'
-                                        value={this.state.password}
-                                        onChange={this.onChange}
-                                    />
-                                </div>
-                                <input type="submit" className="btn btn-outline-warning btn-block mt-4" />
-                                <Link to="/" className="btn btn-outline-warning mt-4">
-                                    Cancel
-                                </Link>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    console.log(localStorage.getItem('user'));
+    return (
+      <div className="Login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <br />
+              <h2 className="display-4 text-center">Login</h2>
             </div>
-        );
-    }
+
+            <div className="col-md-4 m-auto">
+              <p className="lead text-center">
+
+              </p>
+              <form noValidate onSubmit={this.onSubmit}>
+                <div className='form-group'>
+                  <input
+                    autocomplete="off"
+                    type='email'
+                    placeholder='Email'
+                    name='email'
+                    className='form-control'
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='password'
+                    placeholder='Password'
+                    name='password'
+                    className='form-control'
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input type="submit" className="btn btn-outline-warning btn-block mt-4" />
+                <Link to="/" className="btn btn-outline-warning mt-4">
+                  Cancel
+                </Link>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
 
 }
